a503d2e4b58f853ad762514e6ba78a4f84e9b10b
Hsu <hsusa@nih.gov>
2022-11-03 09:41:04 -0400

    First commit for Grant 2.0 Neo4j database

--- nih_grant/annotate/annotation_preprocess_grant.py
+++ nih_grant/annotate/annotation_preprocess_grant.py
@@ -6,29 +6,22 @@ ENCODING = 'latin1'
 
 
 # Get CSV files lists from projects and abstracts folders
-projects_path = "../../data_neo4j/projects_RD"
+projects_path = "../../data_neo4j/projects"
 projects_files = glob.glob(projects_path + "/*.csv")
 
-abstracts_path = "../../data_neo4j/abstracts_RD"
+abstracts_path = "../../data_neo4j/abstracts"
 abstracts_files = glob.glob(abstracts_path + "/*.csv")
-
-
-print(projects_files)
-print(abstracts_files)
 
 
 for projects_file, abstracts_file in zip(projects_files, abstracts_files):
 
-    print(projects_file)
-    print(abstracts_file)
-    print("\n")
-#    annotate_text = prepare_phr_aim(projects_file, abstracts_file)
+    annotate_text = prepare_phr_aim(projects_file, abstracts_file)
     
-#    yr_idx = len(projects_path) + 13
-#    year = projects_file[yr_idx:yr_idx+4]
+    yr_idx = len(projects_path) + 13
+    year = projects_file[yr_idx:yr_idx+4]
     
-#    output_file = "../database/annotation_files/annotation_text_" + year + ".csv" 
-#    annotate_text.to_csv(output_file, index=False, encoding=ENCODING)
+    output_file = "../../data_neo4j/annotation_files/annotation_text_" + year + ".csv" 
+    annotate_text.to_csv(output_file, index=False, encoding=ENCODING)
 
     print("Finished", output_file)
 
