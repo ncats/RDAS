6bef32c4b723b5737509622dc51a55ddd3ba2f53
devonleadman <98187057+devonleadman@users.noreply.github.com>
2022-11-07 15:51:26 -0500

    Full neo4j (#28)
    * new structure setup
    
    * Autoupdate framework created
    
    * Close function
    
    * Database individual update
    
    * Revert "Database individual update"
    
    This reverts commit 58639cadd91e90cddf98ba394cad15cdae9fed44.
    
    * Database Autoupdate for Individual DB

--- new/AlertCypher.py
+++ new/AlertCypher.py
@@ -8,7 +8,8 @@ class AlertCypher ():
         init = os.path.join(workspace, 'config.ini')
         configuration = configparser.ConfigParser()
         configuration.read(init)
-
+        
+        # Connects to neo4j databasej
         server_uri = configuration.get("CREDENTIALS", "neo4j_uri")
         user = configuration.get("CREDENTIALS", "neo4j_username")
         password = configuration.get("CREDENTIALS", "neo4j_password")
@@ -17,8 +18,10 @@ class AlertCypher ():
         self.session = connection.session(database=db)
 
     def run(self, query):
+        # Runs and returns cypher query
         response = self.session.run(query)
         return response
     
     def close(self):
+        # Closes connection to database
         self.session.close()
