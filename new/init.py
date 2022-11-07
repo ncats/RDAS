b3a91ef9a19085c18c8eb4137ca4c646d115dc77
Devon Leadman <shanmardev@hotmail.com>
2022-11-07 12:25:54 -0500

    Autoupdate framework created

--- new/init.py
+++ new/init.py
@@ -1,26 +1,58 @@
 from neo4j import GraphDatabase
 import clinical.generate, grant.generate, pubmed.generate
+from AlertCypher import AlertCypher
+import threading
 import configparser
 import datetime
 from time import sleep
 import sys
 import os
 
-print("[RARE DISEASE ALERT SYSTEM]\n")
+print("[RARE DISEASE ALERT SYSTEM]")
 
-"""
-In this section, check to see if all the databases exist on the server, if not, call each missing generate script with empty=true
-
-
-
-
-
-"""
-
+# Load configuration information
+workspace = os.path.dirname(os.path.abspath(__file__))
+init = os.path.join(workspace, 'config.ini')
 configuration = configparser.ConfigParser()
-configuration.read("config.ini")
-last_run = configuration.get('database','database_last_run')
+configuration.read(init)
 
+# Setup individual database communication objects. AlertCypher object is used to send cypher queries to a specific database in the server
+CTcypher = AlertCypher("clinical")
+GNTcypher = AlertCypher("grant")
+PMcypher = AlertCypher("pubmed")
+
+# Checks if clinical trial database is empty. If it is, it creates it from scratch on a seperate thread
+try:
+    response = CTcypher.run("MATCH (x) RETURN x LIMIT 1").single()
+    if response == None:
+        CT_thread = threading.Thread(target=clinical.generate.check, args=(True,), daemon=True)
+        CT_thread.start()
+        CT_thread.join()
+except:
+    print("[CLINICAL TRIAL] Error finding Neo4j database. Check to see if database exists and rerun script")
+
+# Checks if NIH grant database is empty. If it is, it creates it from scratch on a seperate thread
+try:
+    response = GNTcypher.run("MATCH (x) RETURN x LIMIT 1").single()
+    if response == None:
+        GNT_thread = threading.Thread(target=grant.generate.check, args=(True,), daemon=True)
+        GNT_thread.start()
+        GNT_thread.join()
+except:
+    print("[GRANT] Error finding Neo4j database. Check to see if database exists and rerun script")
+
+# Checks if PubMed database is empty. If it is, it creates it from scratch on a seperate thread
+try:
+    response = PMcypher.run("MATCH (x) RETURN x LIMIT 1").single()
+    if response == None:
+        PM_thread = threading.Thread(target=pubmed.generate.check, args=(True,), daemon=True)
+        PM_thread.start()
+        PM_thread.join()
+except:
+    print("[PUBMED] Error finding Neo4j database. Check to see if database exists and rerun script")
+
+# Gets last database update from configuration file
+last_run = configuration.get("DATABASE","database_last_run")
 if last_run == "":
     start_time = datetime.date.today()
     start_time = start_time.strftime("%m/%d/%y")
@@ -29,6 +61,7 @@ else:
     start_time = last_run.strftime("%m/%d/%y")
     start_time = datetime.datetime.strptime(start_time,"%m/%d/%y")
 
+# Starts a database update every interval of days
 while True:
     current_time = datetime.date.today()
     current_time = current_time.strftime("%m/%d/%y")
@@ -40,6 +73,6 @@ while True:
         grant.generate.check()
         pubmed.generate.check()
 
-    sleep(3600)
     print("Days Since Last Update:\n{day} Days\n".format(day=str(delta.days)))
+    sleep(3600)
     
--- new/pubmed/generate.py
+++ new/pubmed/generate.py
@@ -1,16 +1,19 @@
 import configparser
 import datetime
+import os
 
 def check (empty=False, date=datetime.date.today()):
+    workspace = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    init = os.path.join(workspace, 'config.ini')
     configuration = configparser.ConfigParser()
-    configuration.read("config.ini")
+    configuration.read(init)
     
     if empty == True:
         create()
-        configuration.set('database', 'database_last_run', date.strftime("%m/%d/%y"))
+        configuration.set('DATABASE', 'database_last_run', date.strftime("%m/%d/%y"))
     elif empty == False:
         update()
-        configuration.set('database', 'database_last_run', date.strftime("%m/%d/%y"))
+        configuration.set('DATABASE', 'database_last_run', date.strftime("%m/%d/%y"))
     else:
         print("[ERROR] generate.py \"empty\" parameter not boolean")
 
