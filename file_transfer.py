c1419a540f844c1baccccaed5cc58b182e7ae9cf
devonleadman <shanmardev@hotmail.com>
2023-09-20 16:32:45 -0400

    Production v1.3 [Setup for full file automation]

--- file_transfer.py
+++ file_transfer.py
@@ -13,7 +13,6 @@ import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-dir", "--dump_dir", dest = "dump_dir", help="directory name within the directory that stores the dump files")
-parser.add_argument("-f", "--file", dest = "dump_filename", help="Specific file name")
 parser.add_argument("-a", "--all", dest = "transfer_all", action='store_true', help="transfer all of most recent dumps")
 parser.add_argument("-s", "--server", dest = "server", help="Server name {test/prod}")
 args = parser.parse_args()
@@ -25,8 +24,7 @@ def get_latest_file (path):
 
 def transfer (dump_path=None,dump_dir=None,server=None):
     print(dump_path, dump_dir)
-    p = Popen(['sudo', 'scp', f'{dump_path}', f'{sysvars.current_user}@{server}:/home/{sysvars.current_user}/alert/transfer/{dump_dir}.dump'], encoding='utf8') #WILL BE CHANGED TO TRANSFER FOLDER WITHIN RDAS CODE BASE
-
+    p = Popen(['sudo', 'scp', f'{dump_path}', f'{sysvars.current_user}@{server}:{sysvars.transfer_path}{dump_dir}.dump'], encoding='utf8')
     #WRITE CODE THAT RESPONDS TO PROMPTS TO AUTOMATE TRANSFER
     p.wait()
 
@@ -36,42 +34,34 @@ except KeyError:
     print('REQUIRED SERVER ARGUMENT [use -s {test/prod} or --server {test/prod}]')
 
 if server == sysvars.rdas_urls['test']:
-    dump_dirs = sysvars.dump_dirs_test
-    dump_path = sysvars.dump_path_test
+    dump_path = sysvars.backup_path
 elif server == sysvars.rdas_urls['prod']:
-    dump_dirs = sysvars.dump_dirs_prod
-    dump_path = sysvars.dump_path_prod
+    dump_path = sysvars.transfer_path
 
 if args.transfer_all: # IF TRANSFERING ALL DUMP FILES
-    if args.dump_filename:
-        print('[ERROR] Cannot manually select files to transfer if transfering all')
+    print('TRANSFERING MOST RECENTLY CREATED DUMP FILE FOR EACH DATABASE')
+    if args.server == 'prod':
+        for dump_dir in sysvars.dump_dirs:
+            from_path = f'{dump_path}{dump_dir}.dump'
+            transfer(dump_path=from_path,dump_dir=dump_dir,server=server)
     else:
-        print('TRANSFERING MOST RECENTLY CREATED DUMP FILE FOR EACH DATABASE')
-        if args.server == 'prod':
-            for dump_dir in dump_dirs:
-                from_path = f'{dump_path}{dump_dir}.dump'
-                transfer(dump_path=from_path,dump_dir=dump_dir,server=server)
-        else:
-            for from_dump_dir, to_dump_dir in dump_dirs.items():
-                path = f'{sysvars.dump_path}{from_dump_dir}/*.dump'
-                transfer_path = get_latest_file(path)
-                transfer(dump_path=transfer_path,dump_dir=to_dump_dir,server=server)
+        for dump_dir in sysvars.dump_dirs:
+            path = f'{dump_path}{dump_dir}/*.dump'
+            print(path)
+            transfer_path = get_latest_file(path)
+            transfer(dump_path=transfer_path,dump_dir=dump_dir,server=server)
 
 else: # IF TRANSFERING A SINGLE DUMP FILE
-    if args.dump_dir and args.dump_filename:
-        print('TRANSFERING SPECIFIC DUMP FILE')
-        to_dump_dir = dump_dirs[args.dump_dir]
-        transfer_path = f'{sysvars.dump_path}{args.dump_dir}/{args.dump_filename}'
-        print(transfer_path)
-        transfer(dump_path=transfer_path,dump_dir=to_dump_dir,server=server)
-
-    elif args.dump_dir and not args.dump_filename:
+    if args.dump_dir:
         print('TRANSFERING LATEST DUMP FILE FOR SPECIFIC DATABASE')
-        to_dump_dir = dump_dirs[args.dump_dir]
-        path = f'{sysvars.dump_path}{args.dump_dir}/*.dump'
-        transfer_path = get_latest_file(path)
-        print(transfer_path)
-        transfer(dump_path=transfer_path,dump_dir=to_dump_dir,server=server)
+        if args.server == 'prod':
+            path = f'{dump_path}{args.dump_dir}.dump'
+            transfer(dump_path=transfer_path,dump_dir=args.dump_dir,server=server)
+
+        else:
+            path = f'{dump_path}{args.dump_dir}/*.dump'
+            transfer_path = get_latest_file(path)
+            transfer(dump_path=transfer_path,dump_dir=args.dump_dir,server=server)
 
     else:
         print('ERRORS WITHIN ARGUMENTS')
--- sysvars.py
+++ sysvars.py
@@ -1,16 +1,15 @@
 import os
 
-current_version = 1.2
+current_version = 1.3
 
-current_user = os.environ['LOGNAME']
+current_user = ''
 base_directory_name = 'alert'
 base_path = '/home/{current_user}/{base_directory_name}/'.format(current_user=current_user, base_directory_name=base_directory_name)
 
-dump_path_test = '/home/{current_user}/dumps/'.format(current_user=current_user)
-dump_path_prod = '{base_path}transfer/'.format(base_path=base_path)
+backup_path = '{base_path}backup/'.format(base_path=base_path)
+transfer_path = '{base_path}transfer/'.format(base_path=base_path)
 
-dump_dirs_test = {'clinicaltrials':'clinical','publication':'pubmed','grant':'grant','gard':'gard'} #FROM:TO
-dump_dirs_prod = ['clinical','pubmed','grant','gard']
+dump_dirs = ['clinical','pubmed','grant','gard']
 
 ct_files_path = '{base_path}/clinical/src/'.format(base_path=base_path)
 pm_files_path = '{base_path}/pubmed/src/'.format(base_path=base_path)
