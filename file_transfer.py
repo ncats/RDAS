714c4eff7b9cb0ba079a03635180732253224789
devonleadman <shanmardev@hotmail.com>
2023-10-02 15:14:24 -0400

    Production v1.3.10 [file transfer fixes/added EPI API submodule]

--- file_transfer.py
+++ file_transfer.py
@@ -14,7 +14,8 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument("-dir", "--dump_dir", dest = "dump_dir", help="directory name within the directory that stores the dump files")
 parser.add_argument("-a", "--all", dest = "transfer_all", action='store_true', help="transfer all of most recent dumps")
-parser.add_argument("-s", "--server", dest = "server", help="Server name {test/prod}")
+parser.add_argument("-s", "--server", dest = "server", help="Server name to transfer to {test/prod}")
+parser.add_argument("-f", "--single_file", dest = "single_file", help="full name of file if only transfering a single specific file [requires -dir and -s]")
 args = parser.parse_args()
 
 def get_latest_file (path):
@@ -24,7 +25,7 @@ def get_latest_file (path):
 
 def transfer (dump_path=None,dump_dir=None,server=None):
     print(dump_path, dump_dir)
-    p = Popen(['sudo', 'scp', f'{dump_path}', f'{sysvars.current_user}@{server}:{sysvars.transfer_path}{dump_dir}.dump'], encoding='utf8')
+    p = Popen(['scp', f'{dump_path}', f'{sysvars.current_user}@{server}:{sysvars.transfer_path}{dump_dir}.dump'], encoding='utf8')
     #WRITE CODE THAT RESPONDS TO PROMPTS TO AUTOMATE TRANSFER
     p.wait()
 
@@ -50,6 +51,16 @@ if args.transfer_all: # IF TRANSFERING ALL DUMP FILES
             print(path)
             transfer_path = get_latest_file(path)
             transfer(dump_path=transfer_path,dump_dir=dump_dir,server=server)
+
+elif args.single_file:
+    print("TRANSFERING SINGLE FILE")
+    if args.dump_dir:
+        path = f'{dump_path}{args.dump_dir}/{args.single_file}'
+        transfer(dump_path=path,dump_dir=args.dump_dir,server=server)
+
+    else:
+        path = f'{dump_path}{args.single_file}.dump'
+        transfer(dump_path=path,dump_dir=args.dump_dir,server=server)
 
 else: # IF TRANSFERING A SINGLE DUMP FILE
     if args.dump_dir:
---
+++ removethis.py
@@ -0,0 +1,4 @@
+from subprocess import *
+
+p = Popen(["ssh", "leadmandj@ncats-neo4j-lnx-test1.ncats.nih.gov"])
+print(p)
