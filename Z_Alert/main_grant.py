1362b8eee16afa003fe744681474cbef793c1690
tzhao <tongan.zhao@nih.gov>
2026-09-01 10:59:27 -0400

    Fix: update grant pipeline publication classification import to local task module

--- Z_Alert/main_grant.py
+++ Z_Alert/main_grant.py
@@ -124,7 +124,7 @@ class GrantPipelineRunner(PipelineRunnerBase):
         self._run_pipeline_task(GrantPublicationArticleImportTask)
 
         ''' Re-use '''
-        from pipelines.pipeline_3_publication.task_publication_2 import PublicationEpiNhsClassificationTask
+        from pipelines.pipeline_3_publication.task_publication_2_local import PublicationEpiNhsClassificationTask
         self._run_pipeline_task(PublicationEpiNhsClassificationTask)
 
 
